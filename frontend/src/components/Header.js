38da0f9bc278904b617399a0883bcdaaf979647f
ashleyjihn <2020181001@yonsei.ac.kr>
2023-05-27 19:00:25 +0900

    css update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,8 +2,10 @@ import { Main } from "./Main";
 
 const Header = () => {
     return(
-        <header className="header">
+        <header className="header1">
             <h5>ArtVenture Seoul</h5>
+            <header className="header2"></header>
+            <header className="header3"></header>
             <Main/>
         </header>
     )
